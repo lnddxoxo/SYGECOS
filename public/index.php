a424af13712b8bf5f74f32097ec3842dfe08a2dc
Xoxo lnd <axellelogbo04@gmail.com>
2025-05-28 00:07:13 +0000

    tchiee

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
 
-require_once __DIR__ . '../src/Controller/LoginController.php';
+require_once __DIR__ . '/../src/Controller/LoginController.php';
 
 $loginController = new LoginController();
 
